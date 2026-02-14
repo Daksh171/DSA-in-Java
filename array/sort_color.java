00707e212adaff2f1fbe8cafee41abbff391ae1a
Daksh171 <dakshvaskar4074@gmail.com>
2026-02-14 15:12:18 +0530

    color question solve using insertion sort

---
+++ array/sort_color.java
@@ -0,0 +1,22 @@
+class sort_color{
+    public static void color(int arr[]){
+        for(int i=1;i<arr.length;i++){
+            int curr=arr[i];
+            int prev=i-1;
+            while (prev>=0 && arr[prev]>curr) {
+                arr[prev+1]=arr[prev];
+                prev--;
+            }
+            arr[prev+1]=curr;
+        }
+    }
+    public static void main(String[] args) {
+        int arr[]= {2,2,0,1,1};
+        color(arr);
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+        
+    }
+}
