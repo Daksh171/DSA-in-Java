e71db14a857e69e7f86482ef86be54394f317208
Daksh171 <dakshvaskar4074@gmail.com>
2026-03-19 15:50:14 +0530

    bubble_sort

---
+++ sorting_algo/bubble_sort.java
@@ -0,0 +1,27 @@
+package sorting_algo;
+
+public class bubble_sort {
+    public static void bubble(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            for(int j=i+1;j<arr.length;j++){
+                if(arr[i]>arr[j]){
+                    int temp=arr[j];
+                    arr[j]=arr[i];
+                    arr[i]=temp;
+                }
+            }
+        }
+    }
+    public static void print(int arr[]){
+        for(int i=0;i<arr.length;i++){
+            System.out.print(arr[i]+" ");
+        }
+        System.out.println();
+    }
+    public static void main(String[] args) {
+        int arr[]= {9,5,8,3,1};
+        bubble(arr);
+        print(arr);
+    }
+    
+}
